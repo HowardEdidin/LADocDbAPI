0b21073e6a78633f3505e175c9513803935fe845
Howard Edidin <hedidin@edidingroup.com>
2017-07-17 12:14:34 -0500

    Add files via upload

---
+++ LADocDbAPI/App_Start/WebApiConfig.cs
@@ -0,0 +1,37 @@
+﻿#region
+
+using System.Web.Http;
+using System.Web.Http.Cors;
+using System.Web.Http.ExceptionHandling;
+using Elmah.Contrib.WebApi;
+
+#endregion
+
+namespace LADocDbAPI
+{
+    /// <summary>
+    /// </summary>
+    public static class WebApiConfig
+    {
+        /// <summary>
+        /// </summary>
+        /// <param name="config"></param>
+        public static void Register(HttpConfiguration config)
+        {
+            // Web API configuration and services
+            config.Services.Add(typeof(IExceptionLogger), new ElmahExceptionLogger());
+            // added Cors support ( Origins, headers, methods)
+            var cors = new EnableCorsAttribute("http://localhost:17614", "*", "*");
+            config.EnableCors(cors);
+
+            // Web API routes
+            config.MapHttpAttributeRoutes();
+
+            config.Routes.MapHttpRoute(
+                "DefaultApi",
+                "api/{controller}/{id}",
+                new {id = RouteParameter.Optional}
+                );
+        }
+    }
+}
